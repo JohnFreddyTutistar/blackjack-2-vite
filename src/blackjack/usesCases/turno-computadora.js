ff370b0538a0a4f1bdd05407c37ea7b2c932b98d
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-08-28 11:27:20 -0500

    finalizando refactorizacion

---
+++ src/blackjack/usesCases/turno-computadora.js
@@ -0,0 +1,45 @@
+import { crearCartaHTML } from "./crear-carta-html";
+import { pedirCarta } from "./pedir-carta";
+import { valorCarta } from "./valor-carta"; 
+
+/**
+ * turno de la computadora
+ * @param {Number} puntosMinimos puntos minimos que la computadora necesita pra ganar 
+ * @param {HTMLElement} puntosHTML elemento html para mostrar los puntos
+ * @param {Array<string>} deck
+ */
+export const turnoComputadora = ( puntosMinimos, puntosHTML, divCartasComputadora, deck = [] ) => {
+
+    if( !puntosMinimos ) throw new Error('Puntos minimos son necesarios')
+    if( !deck ) throw new Error('El deck es necesario')
+
+    let puntosComputadora = 0;
+
+    do {
+        const carta = pedirCarta( deck );
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML.innerText = puntosComputadora;
+        
+        // <img class="carta" src="assets/cartas/2C.png">
+        const imgCarta = crearCartaHTML( carta )
+        divCartasComputadora.append( imgCarta );
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+}
